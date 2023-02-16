1d772f2c34dae5a6c20690496922b89f9d339c2c
Amit Amanna <amit.natheniel@gmail.com>
2023-02-16 17:20:32 +0100

    First commit (+ playing around with threejs)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Ref} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Canvas} from "@react-three/fiber";
+import {Physics, useBox, usePlane} from "@react-three/cannon";
+import {OrbitControls, Stars} from "@react-three/drei";
+
+function Box() {
+  const [ref, api]: any = useBox(() => ({mass: 1, position: [0,10,0]}));
+  return (<mesh onClick={() => api.velocity.set(0,2,0)} ref={ref}>
+    <boxBufferGeometry attach={'geometry'} />
+    <meshLambertMaterial attach={'material'} color={'hotpink'} />
+  </mesh>)
+}
+
+function Plane() {
+  const [ref]: any = usePlane(() => ({mass: 0, position: [0,0,0], rotation: [-Math.PI/2,0,0]}));
+  return (<mesh ref={ref}>
+    <planeBufferGeometry attach={'geometry'} args={[100,100]}/>
+    <meshLambertMaterial attach={'material'} color={'lightblue'} />
+  </mesh>)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <Canvas>
+    <OrbitControls/>
+    <Stars/>
+    <ambientLight intensity={0.5}/>
+    <spotLight position={[10,15,10]} angle={0.3}/>
+    <Physics>
+      <Box/>
+      <Plane/>
+    </Physics>
+  </Canvas>
+  )
 }
 
 export default App;
